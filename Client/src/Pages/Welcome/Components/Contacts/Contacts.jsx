1f2a115a9bc795fea5b3ee1fe3cc93e8f6205bec
Jonna <yonazyonaz8@gmail.com>
2025-11-25 22:21:01 +0300

    Finished welcome page setup

---
+++ Client/src/Pages/Welcome/Components/Contacts/Contacts.jsx
@@ -0,0 +1,65 @@
+import CallImage from '../../../../assets/Images/Welcome/call.png';
+import FacebookIcon from '../../../../assets/Icons/Welcome/facebook.png';
+import YoutubeIcon from '../../../../assets/Icons/Welcome/youtube.png';
+import InstagramIcon from '../../../../assets/Icons/Welcome/instagram.png';
+import TiktokIcon from '../../../../assets/Icons/Welcome/tik-tok.png';
+import './Contacts.css';
+
+
+function Contacts() {
+        const links = [ {name: "facebook", icon: FacebookIcon},  {name: "youtube", icon: YoutubeIcon}, {name: "instagram",  icon: InstagramIcon}, {name: "tik tok",  icon: TiktokIcon}];
+
+        return (
+                <div className="contacts">
+                        <div className="image-headlines">
+                                <p className="image">
+                                        <img src={CallImage} alt="Team picture" />
+                                </p>
+
+                                <div className="headline-link">
+                                        <p className="headline">
+                                                Meet the Team!
+                                        </p>
+
+                                        <div className="links">
+                                               { links.map((link, index) => (
+                                                        <p key={index} >
+                                                                <img src={link.icon} alt={link.name} />
+                                                        </p>
+                                               ))}
+                                        </div>
+                                </div>
+                        </div>
+
+                        <div className="form-links">
+                                <form action="">
+                                        <p className="text">
+                                                Do you have something to say? Reach out.
+                                        </p>
+
+                                        <p className="name">
+                                                <input type="text" name="name" id="name" placeholder="Name" />
+                                        </p>
+
+                                        <p className="email">
+                                                <input type="email" name="email" id="email" placeholder="Email" />
+                                        </p>
+
+                                        <p className="subject">
+                                                <input type="text" name="subject" id="subject" placeholder="Subject" />
+                                        </p>
+
+                                        <p className="message">
+                                                <textarea name="message" id="message" placeholder="Message..."></textarea>
+                                        </p>
+
+                                        <button type="submit">Submit</button>
+                                </form>
+
+                                <div className="links"></div>
+                        </div>
+                </div>
+        );
+}
+
+export default Contacts;
--- Client/src/Pages/Welcome/Welcome.jsx
+++ Client/src/Pages/Welcome/Welcome.jsx
@@ -1,6 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 import NavBar from "./Components/NavBar/NavBar";
 import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Features from './Components/Features/Features';
+import Contacts from './Components/Contacts/Contacts';
 import Footer from "./Components/Footer/Footer";
 import './Welcome.css';
 
@@ -12,9 +15,9 @@ function Welcome() {
 
                         <Routes>
                                 <Route path={'/'} element={<Home />}></Route>
-                                <Route path={'/about'} element={<div>About</div>}></Route>
-                                <Route path={'/features'} element={<div>Features</div>}></Route>
-                                <Route path={'/contacts'} element={<div>Contacts</div>}></Route>
+                                <Route path={'/about'} element={<About />}></Route>
+                                <Route path={'/features'} element={<Features />}></Route>
+                                <Route path={'/contacts'} element={<Contacts />}></Route>
                         </Routes>
 
                         <Footer />
